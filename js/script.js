ac2cce186b4d4a6e754b9e89fc3d8140b79e0570
Dwarika202249 <dcchand162208@gmail.com>
2020-07-24 21:41:44 +0530

    Magical Restro

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// console.log("This is script.js");
+
+let sendBtn = document.getElementById('sendBtn');
+sendBtn.addEventListener('click', function (e) {
+    alert('Your Complain is Successfully Sent.');
+})
+
+let bookBtn = document.getElementById('bookBtn');
+bookBtn.addEventListener('click', function (e) {
+        alert('Your Booking is Successfully Done.');
+})
